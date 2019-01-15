0d5f947c56861c8743660736fbdd4e0626dcbcdc
Fred Turkington <fred@tenforwardconsulting.com>
2019-01-15 15:44:27 -0600

    Testing fix for cap deployment

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module DevopsFakeclient
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.secrets.secret_key_base = 'asdfasdf'
+    config.credentials.secret_key_base = 'asdfasdf'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
